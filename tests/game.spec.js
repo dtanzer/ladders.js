83dd4294577321d55370805198ffac91f1b7ab06
David Tanzer <business@davidtanzer.net>
2019-02-25 16:37:29 +0100

    working

--- tests/game.spec.js
+++ tests/game.spec.js
@@ -31,31 +31,43 @@ describe('Game', () => {
 	});
 	it('moves players with roll 1', () => {
 		const game = new Game(2);
+
 		game.takeTurn();
+		
 		expect(game.positionOf(1)).toBe(2);
 		expect(game.positionOf(2)).toBe(1);
 	});
 	it('moves players with roll 1 on taking turns', () => {
 		const game = new Game(2);
+
 		game.takeTurn();
 		game.takeTurn();
 		game.takeTurn();
+
 		expect(game.positionOf(1)).toBe(3);
 		expect(game.positionOf(2)).toBe(2);
 	});
 	it('marks first player at end "4" as winner', () => {
 		const game = new Game(1);
+		
 		game.takeTurn();
 		game.takeTurn();
 		game.takeTurn();
+
 		expect(game.winner()).toBe(1);
 	});
 	it('has no winner below end "4"', () => {
 		const game = new Game(1);
-		expect(game.winner()).toBeUndefined();
+
 		game.takeTurn();
-		expect(game.winner()).toBeUndefined();
 		game.takeTurn();
+
 		expect(game.winner()).toBeUndefined();
 	});
 });
+
+// Diese kleinen Schritte, ich comitte mehrmals pro Test.
+// Die assertion kommt am schluss, ist das gut?
+// Tests entstehen schrittweise.
+// Refactoren oft auf die Lösung und dann den Test hinzufügen für allgemeinen Code.
+// 
