d39aab32d6f8a338dd49a5f5d3969ef3a5d4f0a7
David Tanzer <business@davidtanzer.net>
2019-02-25 16:08:54 +0100

    working

--- tests/game.spec.js
+++ tests/game.spec.js
@@ -1,11 +1,15 @@
 class Game {
 	constructor(numberOfPlayers) {
-		
+
+	}
+	positionOf(numberOfPlayer) {
+		return 1;
 	}
 }
 
 describe('Game', () => {
 	it('all players starts game at position 1', () => {
 		const game = new Game(2);
+		expect(game.positionOf(1)).toBe(1);
 	});
 });
