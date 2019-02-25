e919ed900c6f355d06b2af0c8af85f1c836a4b76
David Tanzer <business@davidtanzer.net>
2019-02-25 16:20:43 +0100

    working

--- tests/game.spec.js
+++ tests/game.spec.js
@@ -11,7 +11,7 @@ class Game {
 	}
 	takeTurn() {
 		this.players[this.currentPlayer] += 1;
-		
+
 		this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
 	}
 }
@@ -22,13 +22,13 @@ describe('Game', () => {
 		expect(game.positionOf(1)).toBe(1);
 		expect(game.positionOf(2)).toBe(1);
 	});
-	it('moves players', () => {
+	it('moves players with roll 1', () => {
 		const game = new Game(2);
 		game.takeTurn();
 		expect(game.positionOf(1)).toBe(2);
 		expect(game.positionOf(2)).toBe(1);
 	});
-	it('moves players on turns', () => {
+	it('moves players with roll 1 on taking turns', () => {
 		const game = new Game(2);
 		game.takeTurn();
 		game.takeTurn();
