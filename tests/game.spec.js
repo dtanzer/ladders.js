655ae7289a36c90d3dc19705112956d3519eb36f
David Tanzer <business@davidtanzer.net>
2019-02-25 16:09:15 +0100

    working

--- tests/game.spec.js
+++ tests/game.spec.js
@@ -11,5 +11,6 @@ describe('Game', () => {
 	it('all players starts game at position 1', () => {
 		const game = new Game(2);
 		expect(game.positionOf(1)).toBe(1);
+		expect(game.positionOf(2)).toBe(1);
 	});
 });
